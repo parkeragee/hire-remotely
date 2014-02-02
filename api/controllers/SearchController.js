5e95eb89263e25265fab8060dd00832055bdcbab
Parker Agee <parkeragee4744@gmail.com>
2014-02-02 08:47:30 -0600

    deeper search

--- api/controllers/SearchController.js
+++ api/controllers/SearchController.js
@@ -2,18 +2,94 @@ module.exports = {
 	
 	post: function (req, res, next) {
 		var searchQuery = req.param('searchQuery');
-		// Post.find({
-			// jobDescription: {
-			// 	contains: searchQuery
-			// }
-		// });
-		Post.find()
-		.where({jobDescription: {contains: searchQuery}})
-		.sort({createdAt: 'desc'})
-		.exec(function (err, results) {
-			res.view({
-				results: results
+		var profession = req.param('profession');
+		var specialty = req.param('specialty');
+		if(profession == undefined && specialty == undefined) {
+			Post.find()
+			.where({
+				or: [
+					{jobDescription: {contains: searchQuery}},
+					{jobTitle: {contains: searchQuery}},
+					{companyName: {contains: searchQuery}},
+					{city: {contains: searchQuery}},
+					{state: {contains: searchQuery}}
+				]
+			})
+			.sort({createdAt: 'desc'})
+			.exec(function (err, results) {
+				res.view({
+					results: results
+				});
 			});
-		});
+		} else if (profession !== undefined && specialty == undefined) {
+			Post.find()
+			.where({
+				or: [
+					{jobDescription: {contains: searchQuery}},
+					{jobTitle: {contains: searchQuery}},
+					{companyName: {contains: searchQuery}},
+					{city: {contains: searchQuery}},
+					{state: {contains: searchQuery}}
+				]
+			})
+			.where({
+				profession: profession
+			})
+			.sort({createdAt: 'desc'})
+			.exec(function (err, results) {
+				res.view({
+					results: results
+				});
+			});
+		} else if (profession == undefined && specialty !== undefined) {
+			Post.find()
+			.where({
+				or: [
+					{jobDescription: {contains: searchQuery}},
+					{jobTitle: {contains: searchQuery}},
+					{companyName: {contains: searchQuery}},
+					{city: {contains: searchQuery}},
+					{state: {contains: searchQuery}}
+				]
+			})
+			.where({
+				specialty: specialty
+			})
+			.sort({createdAt: 'desc'})
+			.exec(function (err, results) {
+				res.view({
+					results: results
+				});
+			});
+		} 
+
+		// else if (searchQuery == "") {
+		// 	console.log('No search query');
+		// }
+
+		else {
+			Post.find()
+			.where({
+				or: [
+					{jobDescription: {contains: searchQuery}},
+					{jobTitle: {contains: searchQuery}},
+					{companyName: {contains: searchQuery}},
+					{city: {contains: searchQuery}},
+					{state: {contains: searchQuery}}
+				]
+			})
+			.where({
+				profession: profession
+			})
+			.where({
+				specialty: specialty
+			})
+			.sort({createdAt: 'desc'})
+			.exec(function (err, results) {
+				res.view({
+					results: results
+				});
+			});
+		} 
 	}  
 };
